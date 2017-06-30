6979f364e5ea715fa8a461929490b02601ecffea
hihozhou <hihozhou@gmail.com>
2017-06-30 11:26:22 +0800

    Point Exception

---
+++ src/Exception.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hiho
+ * Date: 17-6-30
+ * Time: 上午11:23
+ */
+
+namespace CV;
+
+/**
+ * Class Exception
+ * @package CV
+ */
+class Exception extends \Exception
+{
+
+}
---
+++ src/Point.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hiho
+ * Date: 17-6-30
+ * Time: 上午11:23
+ */
+
+namespace CV;
+
+/**
+ * Class Point
+ * @package CV
+ * Point对象在OpenCV中用与表示点(x,y)
+ */
+class Point
+{
+    /**
+     * @var int
+     */
+    public $x;
+
+    /**
+     * @var int
+     */
+    public $y;
+
+    /**
+     * Point constructor.
+     * @param int $x
+     * @param int $yq
+     */
+    public function __construct(int $x, int $y)
+    {
+    }
+
+    /**
+     * 用于输出Point对象信息
+     */
+    public function print(){
+
+    }
+}
