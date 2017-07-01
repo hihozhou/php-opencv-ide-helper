b5dfec84979ea80b96b362d9d48e309bb882438c
hihozhou <hihozhou@gmail.com>
2017-07-01 14:19:18 +0800

    addWeighted function

--- src/Functions.php
+++ src/Functions.php
@@ -8,7 +8,8 @@ namespace CV;
  * @param int $flags
  * @return Mat
  */
-function imread(string $path, int $flags = IMREAD_COLOR){
+function imread(string $path, int $flags = IMREAD_COLOR)
+{
 
 }
 
@@ -17,7 +18,8 @@ function imread(string $path, int $flags = IMREAD_COLOR){
  * @param string $windowName 窗口名称
  * @param Mat $mat 需要展示的Mat矩阵
  */
-function imshow(string $windowName, Mat $mat){
+function imshow(string $windowName, Mat $mat)
+{
 
 }
 
@@ -26,6 +28,22 @@ function imshow(string $windowName, Mat $mat){
  * 如果$sec大于0的等待多少秒，如果为0则一直等待
  * @param int $sec 单位秒
  */
-function waitKey(int $sec){
+function waitKey(int $sec)
+{
+
+}
+
+
+/**
+ * 计算矩阵数据加权，用于合成图片
+ * @param Mat $src1 第一张合成图片的Mat对象
+ * @param double $alpha 第一张图片的权重
+ * @param Mat $src2 第二张合成图片的Mat对象，这里注意要与一个Mat对象拥有相同的尺寸和通道数
+ * @param double $beta 第二张图片的权重，$alpha + $beta = 1
+ * @param double $gamma 一个加到权重总和上的标量值。其含义通过接下来的里出的式子自然会理解
+ * @param int $dtype 返回的Mat对象的深度，默认为-1.当两个Mat具有相同的深度时候，这个参数设置为-1,既等同于$src1.depth()
+ */
+function addWeighted(Mat $src1, double $alpha, Mat $src2, double $beta, double $gamma, int $dtype = -1)
+{
 
 }
