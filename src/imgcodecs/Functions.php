96306299b5f9d4c206a4f1523e29674f803d8944
hihozhou <hihozhou@gmail.com>
2018-12-07 18:45:25 +0800

    注释

--- src/imgcodecs/Functions.php
+++ src/imgcodecs/Functions.php
@@ -4,9 +4,12 @@ namespace CV;
 
 /**
  * 读取图片
- * @param string $path 读取的路径
- * @param int $flags
- * @return Mat
+ * @author hihozhou
+ *
+ * @param string $path  需要读取图片的路径
+ * @param int    $flags 读取的方式配置，如：IMREAD_COLOR以BGR3通道，IMREAD_GRAYSCALE灰度形式读取，等。
+ *
+ * @return \CV\Mat 返回存在读取图像数据的Mat对象
  */
 function imread(string $path, int $flags = IMREAD_COLOR)
 {
